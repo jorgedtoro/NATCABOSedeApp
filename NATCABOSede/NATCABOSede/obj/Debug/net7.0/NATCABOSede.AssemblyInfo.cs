891c5ae6b849688f655c585fe2b31451927afd96
Jorge de Toro <jorge.detoro@alten.es>
2024-11-21 13:35:57 +0100

    Add .gitignore file

--- NATCABOSede/NATCABOSede/obj/Debug/net7.0/NATCABOSede.AssemblyInfo.cs
+++ NATCABOSede/NATCABOSede/obj/Debug/net7.0/NATCABOSede.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NATCABOSede")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0152d703ef7db008ace785335ccecd8ccdab17ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("NATCABOSede")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NATCABOSede")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

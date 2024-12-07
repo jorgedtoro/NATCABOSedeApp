e03290eee618ad85b6d1e895e0cdeca40c5fc2a6
Jorge de Toro <jorge.detoro@alten.es>
2024-12-07 19:06:12 +0100

    feat: estructura de estilos en carpetas. Modificación de estilos

--- NATCABOSede/NATCABOSede/obj/Debug/net7.0/NATCABOSede.AssemblyInfo.cs
+++ NATCABOSede/NATCABOSede/obj/Debug/net7.0/NATCABOSede.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NATCABOSede")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a5acb57d04c36182803b06a24f34694bfd47e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e58f53bea30849ba597532390ea9f7e458acec")]
 [assembly: System.Reflection.AssemblyProductAttribute("NATCABOSede")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NATCABOSede")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

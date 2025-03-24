c3c07af4bcf040603e892c9468adaaf4a0087d26
Jorge de Toro <jorgedtoro@gmail.com>
2025-03-24 18:00:58 +0100

    feat: tabla objetivos funcionalidad y front

---
+++ NATCABOSede/NATCABOSede/Models/VwObjetivosConfeccione.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NATCABOSede.Models;
+
+public partial class VwObjetivosConfecciones
+{
+    public string SCode { get; set; } = null!;
+
+    public string SName { get; set; } = null!;
+
+    public short? PercExtraOper { get; set; }
+
+    public double? PpmObj { get; set; }
+
+    public double? ModObj { get; set; }
+
+    public double? FttObj { get; set; }
+}

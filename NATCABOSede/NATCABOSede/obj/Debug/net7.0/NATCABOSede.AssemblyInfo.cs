d9d3ba506cfd561b1fe7fd922224106ce1327440
Jorge de Toro <jorgedtoro@gmail.com>
2025-03-26 19:03:21 +0100

    feat: autoRefresh vista InfoEnvasadoras

--- NATCABOSede/NATCABOSede/obj/Debug/net7.0/NATCABOSede.AssemblyInfo.cs
+++ NATCABOSede/NATCABOSede/obj/Debug/net7.0/NATCABOSede.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NATCABOSede")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff45108e95bc7ec7756b306b86fa3158f3e6a45")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad720641fe9e319d42f8190be9a037194fa96e2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("NATCABOSede")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NATCABOSede")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
